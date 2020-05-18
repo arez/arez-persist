ddbffb0964dbc2c968a8b0d378939e2c8dba8075
Peter Donald <peter@realityforge.org>
2020-05-18 15:13:24 +1000

    Add descriptor for persistent properties

---
+++ processor/src/main/java/arez/persist/processor/PropertyDescriptor.java
@@ -0,0 +1,40 @@
+package arez.persist.processor;
+
+import java.util.Objects;
+import javax.annotation.Nonnull;
+import javax.lang.model.element.ExecutableElement;
+
+final class PropertyDescriptor
+{
+  @Nonnull
+  private final String _name;
+  @Nonnull
+  private final String _store;
+  @Nonnull
+  private final ExecutableElement _getter;
+
+  PropertyDescriptor( @Nonnull final String name, @Nonnull final String store, @Nonnull final ExecutableElement getter )
+  {
+    _name = Objects.requireNonNull( name );
+    _store = Objects.requireNonNull( store );
+    _getter = Objects.requireNonNull( getter );
+  }
+
+  @Nonnull
+  String getName()
+  {
+    return _name;
+  }
+
+  @Nonnull
+  String getStore()
+  {
+    return _store;
+  }
+
+  @Nonnull
+  ExecutableElement getGetter()
+  {
+    return _getter;
+  }
+}
