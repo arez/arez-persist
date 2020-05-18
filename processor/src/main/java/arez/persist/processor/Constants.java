961073f5802270a04b615f6d68317a4711039f3e
Peter Donald <peter@realityforge.org>
2020-05-18 15:14:39 +1000

    Add some arez annotations used during validation

--- processor/src/main/java/arez/persist/processor/Constants.java
+++ processor/src/main/java/arez/persist/processor/Constants.java
@@ -5,6 +5,10 @@ import javax.annotation.Nonnull;
 final class Constants
 {
   @Nonnull
+  static final String AREZ_COMPONENT_CLASSNAME = "arez.annotations.ArezComponent";
+  @Nonnull
+  static final String OBSERVABLE_CLASSNAME = "arez.annotations.Observable";
+  @Nonnull
   static final String PERSIST_TYPE_CLASSNAME = "arez.persist.PersistType";
   @Nonnull
   static final String PERSIST_CLASSNAME = "arez.persist.Persist";
