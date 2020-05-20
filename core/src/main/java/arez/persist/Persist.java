b27d67f0f01f98de68d3d4650026da3034770216
Peter Donald <peter@realityforge.org>
2020-05-21 09:50:39 +1000

    Update javadocs

--- core/src/main/java/arez/persist/Persist.java
+++ core/src/main/java/arez/persist/Persist.java
@@ -14,7 +14,8 @@ public @interface Persist
 {
   /**
    * Return the name used to persist the property.
-   * If not specified, then the name of the observable property will be used.
+   * If unspecified and the method is named according to javabeans getter conventions then the java bean property name
+   * will be used, otherwise the name of the method will be used.
    *
    * <p>It should be noted that production mode persistent properties that are not persisted across
    * reloads will use synthetic keys as an optimization strategy.</p>
